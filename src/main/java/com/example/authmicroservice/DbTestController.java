a6824bfc5e6b8db8fc706cf580325d3fbb6b5cfd
egemen <egemenozyurek@outlook.com>
2025-11-30 02:22:37 +0300

    initial commit

---
+++ src/main/java/com/example/authmicroservice/DbTestController.java
@@ -0,0 +1,26 @@
+package com.example.authmicroservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+@RestController
+public class DbTestController {
+
+    private final DataSource dataSource;
+
+    public DbTestController(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @GetMapping("/testConnection")
+    public String testConnection() {
+        try (Connection connection = dataSource.getConnection()) {
+            return "OK";
+        } catch (Exception e) {
+            return "ERROR: " + e.getMessage();
+        }
+    }
+}
