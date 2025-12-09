abfdb2e87245912011e25c534f45c381a5347f80
egemen <egemenozyurek@outlook.com>
2025-12-09 20:49:08 +0300

    new endpoints requests

--- src/main/java/com/example/authmicroservice/config/SecurityConfig.java
+++ src/main/java/com/example/authmicroservice/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
