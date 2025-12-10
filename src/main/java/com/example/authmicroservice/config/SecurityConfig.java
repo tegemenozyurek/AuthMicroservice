8d45eea6c737b9a48a96393d845d4de428e8b6b5
egemen <egemenozyurek@outlook.com>
2025-12-10 17:41:29 +0300

    create user

--- src/main/java/com/example/authmicroservice/config/SecurityConfig.java
+++ src/main/java/com/example/authmicroservice/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .csrf(AbstractHttpConfigurer::disable)
